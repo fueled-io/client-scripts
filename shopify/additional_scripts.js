121096827e6f314aa6efff5d9a30b5a4fc3f75ac
Eddie Svirsky <1399489+esvirsky@users.noreply.github.com>
2023-12-12 16:23:37 -0700

    FUEL-959 additional script to set the order count and LTV on a customer

---
+++ shopify/additional_scripts.js
@@ -0,0 +1,16 @@
+<script>
+  const dataLayer = window.dataLayer || [];
+  if (!window.dataLayer) {
+    window.dataLayer = dataLayer;
+  }
+
+  dataLayer['fueled'] = {};
+  {% if customer %}
+    dataLayer['fueled']['customer'] = {};
+    {% if customer.orders_count > 0 %}
+        dataLayer['fueled']['customer']['total_spent'] = {{ customer.total_spent | divided_by: 100.0 }};
+        dataLayer['fueled']['customer']['orders_count'] = {{ customer.orders_count }};
+    {% endif %}
+  {% endif %}
+
+</script>
