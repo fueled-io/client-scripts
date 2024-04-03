3a9679f5372e918fac70898c43b4550bc56f456b
Sean Larkin <sean@fueled.io>
2024-04-03 07:56:53 -0600

    Update custom_pixel.js
    Better pixel documentation and defaults.

--- shopify/custom_pixel.js
+++ shopify/custom_pixel.js
@@ -1,11 +1,19 @@
 /**
  * Possible events:
- * product_added_to_cart, checkout_started, checkout_contact_info_submitted, checkout_address_info_submitted, checkout_shipping_info_submitted, payment_info_submitted
+ * product_added_to_cart, checkout_started, checkout_contact_info_submitted, checkout_address_info_submitted, payment_info_submitted
+ *
+ * Note: You might notice a warning message in Shopify's Custom Pixel UI suggesting that this script doesn't include any event subscriptions.
+ * That is not an issue. It's just that our scripts use variables to generate subscription calls.
+ *
+ * You can learn more at: https://learn.fueled.io/apps/shopify/shopify-event-tracking/custom-pixel-for-tracking-checkout-steps.
+ *
  */
 const trackEvents = [
-  'checkout_started',
   //'product_added_to_cart', /* Uncomment this event if you have disabled Fueled's standard "Add to Cart" tracking in our app configuration. */
-  'payment_info_submitted'
+  'checkout_started', /* This event fires an "Initiate Checkout" event for Facebook Pixel/CAPI. */
+  'checkout_contact_info_submitted', /* This event fires an identify() event that improves match data. */
+  'checkout_address_info_submitted', /* This event fires an identify() event that improves match data. */
+  'payment_info_submitted' /* This event fires an "Add Payment Info" event into GA4 and FB. */
 ];
 
 /**
