5f6ab426e6621864e1f66b7e194b90cd33ed146c
aklishchevskiy <aklishchevskiy@gmail.com>
2025-01-07 11:55:54 +0200

    FUEL-1060 Add data layer to pixel v2

--- shopify/custom_pixel_v2.js
+++ shopify/custom_pixel_v2.js
@@ -1,17 +1,84 @@
-(function(d,s) {
-  d.head.appendChild(Object.assign(d.createElement('link'),{
-    href: s,    
-    as: 'script',
-    rel: 'preload'
-  }));
-  d.head.appendChild(Object.assign(d.createElement('script'), {
-    src: s,
-    async: 1
-  }));
+/**
+ * Possible events:
+ * product_added_to_cart, checkout_started, checkout_contact_info_submitted, checkout_address_info_submitted, payment_info_submitted
+ *
+ * Note: You might notice a warning message in Shopify's Custom Pixel UI suggesting that this script doesn't include any event subscriptions.
+ * That is not an issue. It's just that our scripts use variables to generate subscription calls.
+ *
+ * You can learn more at: https://learn.fueled.io/apps/shopify/shopify-event-tracking/custom-pixel-for-tracking-checkout-steps.
+ *
+ */
+const trackEvents = [
+	'checkout_started', /* This event fires an 'Initiate Checkout' event for Facebook Pixel/CAPI. */
+	'checkout_contact_info_submitted', /* This event fires an identify() event that improves match data. */ 
+	'checkout_address_info_submitted', /* This event fires an identify() event that improves match data. */
+	'payment_info_submitted', /* This event fires an 'Add Payment Info' event into GA4 and FB. */
+	'checkout_completed', /* This event fires an Identify event into GA4 and FB. */
+];
 
-  window.addEventListener('fueled-shopify-ready', () => {
-    if (window.fueled && window.fueled.initPixel) {
-      window.fueled.initPixel(api);
-    }
-  })
-})(document, '/apps/fueled/client.js?page=custom_pixel_v2');
+/**
+ * Possible events:
+ * checkout_started
+ */
+const ga4ExcludedEvents = ['checkout_started']; /* Leave this setting as is, unless you have disabled Fueled's server-side event tracking entirely. */
+
+/**
+ * Do not touch any of the code below.
+ */
+
+((configs) => {
+	const events = [];
+	let scriptLoaded = false;
+	let fueledReady = false;
+
+	function loadScript(src) {
+		const script = document.createElement('script');
+		script.src = src;
+		script.async = true;
+
+		const link = document.createElement('link');
+		link.href = src;
+		link.as = 'script';
+		link.rel = 'preload';
+
+		document.head.appendChild(script);
+		document.head.appendChild(link);
+
+		scriptLoaded = true;
+	}
+
+	function trackEvent(event) {
+		const options = configs.ga4ExcludedEvents.includes(event.name) ? {
+				plugins: {
+					all: true,
+					'google-analytics': false,
+				},
+			} : null;
+
+		window.fueled.customPixelV2.trackEvent({ event, options, api });
+	}
+
+	configs.trackEvents.forEach(eventName => {
+		api.analytics.subscribe(eventName, async (event) => {
+			if (!scriptLoaded) {
+				loadScript('/apps/fueled/client.js?page=custom_pixel_v2');
+			}
+
+			if (fueledReady) {
+				trackEvent(event);
+			} else {
+				events.push(event);
+			}
+		});
+	});
+
+	window.addEventListener('fueled-shopify-ready', () => {
+		if (window.fueled && !fueledReady) {
+			fueledReady = true;
+			events.forEach(event => trackEvent(event));
+		}
+	});
+})({
+	trackEvents,
+	ga4ExcludedEvents
+});
