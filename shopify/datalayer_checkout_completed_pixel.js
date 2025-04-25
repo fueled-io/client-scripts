da185946a9e3a8c1f2755d3bcb5a8d98ef43cb57
aklishchevskiy <aklishchevskiy@gmail.com>
2025-04-25 11:20:17 +0300

    FUEL-1976 Move GA4 inside listener

--- shopify/datalayer_checkout_completed_pixel.js
+++ shopify/datalayer_checkout_completed_pixel.js
@@ -1,17 +1,31 @@
-((gtmContainerId) => {
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer', gtmContainerId);
-})('GTM-XXXXXXX');  
+analytics.subscribe("checkout_completed", async (event) => {
+  await new Promise((r, j) => {
+    const w = window;
+    const d = document;
+    const s = "script";
+    const l = "dataLayer";
+    const i = "GTM-XXXXXXX";
 
-analytics.subscribe("checkout_completed", (event) => {
+    w[l] = w[l] || [];
+    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != "dataLayer" ? "&l=" + l : "";
+    j.async = true;
+    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+    j.addEventListener("load", () => {
+      r();
+    });
+    j.addEventListener("error", () => {
+      j();
+    });
+    f.parentNode.insertBefore(j, f);
+  });
+
   const checkout = event.data?.checkout;
   const ordersCount = api.init.data?.customer?.ordersCount || 0;
   const isNewCustomer = checkout.order?.customer?.isFirstOrder || Number(ordersCount) === 1;
-  const coupon = (checkout?.discountApplications || [])
-    .map((item) => item.title).join(",");
+  const coupon = (checkout?.discountApplications || []).map((item) => item.title).join(",");
 
   const customerData = {
     customer_id: checkout.order?.customer?.id ?? null,
@@ -67,7 +81,7 @@ analytics.subscribe("checkout_completed", (event) => {
   });
 
   window.dataLayer = window.dataLayer || [];
-  
+
   // Add additional data to the dataLayer['fueled'] object
   window.dataLayer.fueled = {
     customer: customerData,
@@ -81,5 +95,3 @@ analytics.subscribe("checkout_completed", (event) => {
     fueled: window.dataLayer.fueled,
   });
 });
-
-
